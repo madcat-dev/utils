66e53ebce72ad973df2b9fe39a32fa419db1d559
Yura <virt780@gmail.com>
2022-07-26 13:54:21 +0300

    fix

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -10,10 +10,18 @@ ENDWELCOME
 )
 
 send_notification() {
+    SPATH=${1:-$SAVE_PATH}
+
     notify-send -u NORMAL \
         -i "$SAVE_PATH" \
-        "${SAVE_PATH/$HOME/\~}"
+        "${SPATH/$HOME/\~}"
 }
+
+if ! which xfce4-screenshooter > /dev/null; then
+    echo -e "$WELCOME"
+    echo -e "\033[31mxfce4-screenshooter not found\033[0m"
+    exit 1
+fi
 
 SAVE_PATH="$(mktemp -u "$(xdg-user-dir DESKTOP)/$(date +'%Y-%m-%d').XXXXXX.png")"
 
@@ -29,9 +37,19 @@ case "${1}" in
         ;;
     *)
         echo -e "$WELCOME"
-        exit 1
+        exit 0
         ;;
 esac
 
-xfce4-screenshooter ${CMD/_/} -s "$SAVE_PATH" \
-    && send_notification
+if xfce4-screenshooter ${CMD/_/} -s "$SAVE_PATH"; then
+    if [[ "${2}" == "--clip" ]]; then
+        if which xclip > /dev/null; then
+            xclip -selection clipboard -t image/png -i "$SAVE_PATH"
+            send_notification "Clipboard"
+            rm -f "$SAVE_PATH"
+            exit 0
+        fi
+    fi
+
+    send_notification
+fi
